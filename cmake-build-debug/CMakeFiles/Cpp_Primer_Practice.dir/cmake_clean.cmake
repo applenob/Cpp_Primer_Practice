34c8aef13bb962948fded2188ce8809ac3170fc6
applenob <nobking@126.com>
2017-09-15 15:26:45 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/Cpp_Primer_Practice.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Cpp_Primer_Practice.dir/main.cpp.o"
+  "Cpp_Primer_Practice.pdb"
+  "Cpp_Primer_Practice"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Cpp_Primer_Practice.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Cpp_Primer_Practice.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Cpp_Primer_Practice.
+# This may be replaced when dependencies are built.

e9b26e488e1be9b9c50cb8e074de13ca4a882a07
applenob <applecer@pku.edu.cn>
2018-12-31 16:24:50 +0800

    update

---
+++ cpp_source/ch04/ch04.cpp
@@ -0,0 +1,35 @@
+//
+// chapter 4
+// 表达式
+
+#include <iostream>
+
+using namespace std;
+
+int q_4_28()
+{
+    cout << "bool:\t\t" << sizeof(bool) << " bytes" << endl << endl;
+
+    cout << "char:\t\t" << sizeof(char) << " bytes" << endl;
+    cout << "wchar_t:\t" << sizeof(wchar_t) << " bytes" << endl;
+    cout << "char16_t:\t" << sizeof(char16_t) << " bytes" << endl;
+    cout << "char32_t:\t" << sizeof(char32_t) << " bytes" << endl << endl;
+
+    cout << "short:\t\t" << sizeof(short) << " bytes" << endl;
+    cout << "int:\t\t" << sizeof(int) << " bytes" << endl;
+    cout << "long:\t\t" << sizeof(long) << " bytes" << endl;
+    cout << "long long:\t" << sizeof(long long) << " bytes" << endl << endl;
+
+    cout << "float:\t\t" << sizeof(float) << " bytes" << endl;
+    cout << "double:\t\t" << sizeof(double) << " bytes" << endl;
+    cout << "long double:\t" << sizeof(long double) << " bytes" << endl << endl;
+
+    return 0;
+}
+
+int main(){
+    q_4_28();
+}
+
+
+

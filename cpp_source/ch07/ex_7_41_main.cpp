dfa54c6ece44c27cff920dfe7d3d0df9209ffd6f
applenob <applecer@pku.edu.cn>
2019-01-26 20:00:59 +0800

    finish ch07

---
+++ cpp_source/ch07/ex_7_41_main.cpp
@@ -0,0 +1,23 @@
+#include "ex_7_41.h"
+using std::cout; using std::endl;
+
+int main()
+{
+    cout << "1. default way: " << endl;
+    cout << "----------------" << endl;
+    Sales_data s1;   
+    
+    cout << "\n2. use std::string as parameter: " << endl;
+    cout << "----------------" << endl;
+    Sales_data s2("CPP-Primer-5th");
+    
+    cout << "\n3. complete parameters: " << endl;
+    cout << "----------------" << endl;
+    Sales_data s3("CPP-Primer-5th", 3, 25.8);
+    
+    cout << "\n4. use istream as parameter: " << endl;
+    cout << "----------------" << endl;
+    Sales_data s4(std::cin);
+    
+    return 0;
+}
---
+++ cpp_source/ch07/screen.cpp
@@ -0,0 +1,12 @@
+# include "screen.h"
+
+int main()
+{
+    Screen myScreen(5, 5, 'X');
+    myScreen.move(4, 0).set('#').display(std::cout);
+    std::cout << "\n";
+    myScreen.display(std::cout);
+    std::cout << "\n";
+
+    return 0;
+}
